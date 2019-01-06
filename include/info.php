3547745bd5bd70f8f6915b98c0b635ee461ea02e
KidsXH <wlzeign@hotmail.com>
2019-01-07 02:52:53 +0800

    Update info.php

--- include/info.php
+++ include/info.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: NewbeeWen
- * Date: 2018/12/26
- * Time: 11:53
- */
 $TITLE = '水体提取'; //网页标题
-$LOGIN_MOD = 0; //登录模块 0->OFF 1->ON
 $IMG_PATH = 'uploads'  . DIRECTORY_SEPARATOR; // 图片上传路径
 $TEMP_IMG_FILE = 'temp.tif'; //上传临时文件名称
