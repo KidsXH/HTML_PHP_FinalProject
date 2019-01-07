dd1d16f9577e94d8f15b921928a4cd657b55a9c8
KidsXH <wlzeign@hotmail.com>
2019-01-07 13:23:15 +0800

    Update js.php

--- templates/js.php
+++ templates/js.php
@@ -1,12 +1,12 @@
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="assets/js/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
   <script src="https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
-  <script src="assets/js/purify.min.js" crossorigin="anonymous"></script>
-  <script src="assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  <script src="assets/js/fileinput.min.js" crossorigin="anonymous"></script>
-  <script src="assets/js/theme.min.js" crossorigin="anonymous"></script>
+  <script src="assets/js/purify.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/fileinput.min.js"></script>
+  <script src="assets/js/theme.min.js"></script>
   <script src="assets/js/bootstrap-input-spinner.js"></script>
