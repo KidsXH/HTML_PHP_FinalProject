b895f227972fa341e870cee75c896b4846922897
KidsXH <wlzeign@hotmail.com>
2019-01-07 13:19:02 +0800

    Update css.php

--- templates/css.php
+++ templates/css.php
@@ -1,9 +1,9 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="assets/css/fileinput.min.css" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/all.css" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/style.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/fileinput.min.css">
+  <link rel="stylesheet" href="assets/css/all.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
